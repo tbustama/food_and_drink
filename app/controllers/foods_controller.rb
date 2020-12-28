3a38244886f1d046f6b141c270106e8856fd5f9f
dangnhon <dang.nhon12345@yahoo.com>
2020-12-28 14:09:28 -0800

    user model

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,61 @@
 class FoodsController < ApplicationController
+
+    # <%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %> caused a webpacker error when left in on line 10 within /views.application.html.erb 
+    # Took it out to not have issues, but may need it later on.
+    def index 
+        @foods = Food.all 
+    end 
+
+    def show 
+        @food = Food.find(params[:id])
+    end 
+
+    def new 
+        @food = Food.new 
+    end 
+
+    def create 
+
+        # don't know how to implement the user_id collection select or even if it's need yet
+        #  <%= f.collection_select :user_id, @user, :id, :name %>
+        
+        @food = Food.create(food_params)
+
+        #@food.user_id = session[:user_id]
+        if @food.valid?
+            redirect_to @food 
+        else 
+            flash[:errors] = @food.errors.full_messages
+            redirect_to new_food_path
+        end
+
+    end 
+
+    def edit 
+        @food = Food.find(params[:id])
+    end 
+
+    def update 
+        @food = Food.find(params[:id])
+        @food.update(food_params)
+        if @food.valid?
+            redirect_to @food 
+        else 
+            flash[:errors] = @food.errors.full_messages
+            redirect_to edit_food_path
+    end 
+
+    def destroy 
+        @food = Food.find(params[:id])
+        @food.destroy
+        redirect_to foods_path
+    end 
+
+    private 
+
+    def food_params 
+        params.require(:food).permit(:category, :name, :ingredients, :directions, :image_url, :time, :user_id)
+    end 
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+
+    def show 
+        @user = User.find(params[:id])
+    end 
+
+    def new 
+        @user = User.new 
+    end 
+
+    def create 
+        @user = User.create(user_params)
+        if @user.valid?
+            redirect_to @user
+        else 
+            flash[:errors] = @user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end 
+
+    def edit 
+        @user = User.find(params[:id]) 
+    end 
+
+    def update 
+    end 
+
+    def destroy 
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :city, :password, :username)
+    end 
+
 end
