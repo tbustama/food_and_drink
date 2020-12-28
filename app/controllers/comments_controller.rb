60c4cc67e8f3bf22a2c51190bee47c6f33673ffc
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-28 14:02:22 -0800

    set up the drink and comments fucntionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,50 @@
 class CommentsController < ApplicationController
+    def new
+        @comment = Comment.new
+    end
+
+    def update
+        @comment= Comment.find(params[:id])
+        @comment.update(comment_params)
+	    if @comment.valid?
+        	redirect_to @comment
+	    else
+		    flash[:errors] = @comment.errors.full_messages
+		    redirect_to edit_comment_path
+	    end
+    end
+
+    def create
+        byebug
+        @comment = Comment.create(comment_params)
+        if @comment.valid? 
+		    redirect_to @comment.recipe
+	    else 
+		    flash[:errors] = @comment.errors.full_messages
+		    redirect_to drinks_path
+	    end
+    end
+
+    def edit
+        @comment = Comment.find(params[:id])
+    end
+
+    def show
+        @comment = Comment.find(params[:id])
+    end
+	
+    def index
+	    @comments = Comment.all
+    end
+
+    def destroy 
+	    @comment = Comment.find(params[:id])
+	    @comment.destroy
+	    redirect_to comments_path
+    end 
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content, :user_id, :recipe_id, :recipe_type)
+    end
 end
--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,51 @@
 class DrinksController < ApplicationController
+    def new
+        @drink = Drink.new
+    end
+
+    def update
+        @drink= Drink.find(params[:id])
+        @drink.update(drink_params)
+	    if @drink.valid?
+            redirect_to @drink
+	    else
+	    	flash[:errors] = @drink.errors.full_messages
+	    	redirect_to edit_drink_path
+	    end
+    end
+
+    def create
+        @drink = Drink.create(drink_params)
+        if @drink.valid? 
+		    redirect_to @drink
+	    else 
+		    flash[:errors] = @drink.errors.full_messages
+		    redirect_to new_drink_path
+	    end
+    end
+
+    def edit
+        @drink = Drink.find(params[:id])
+    end
+
+    def show
+        @drink = Drink.find(params[:id])
+        @comment = Comment.new
+        @comments = Comment.all
+    end
+	
+    def index
+	    @drinks = Drink.all
+    end
+
+    def destroy 
+	    @drink = Drink.find(params[:id])
+	    @drink.destroy
+	    redirect_to drinks_path
+    end 
+
+    private
+    def drink_params
+        params.require(:drink).permit(:name, :category, :ingredients, :directions, :image_url)
+    end
 end
